1789994ba9baa00dc876e0eeaebf4e7a95317827
m4ce-w1ndu <16584672+m4ce-w1ndu@users.noreply.github.com>
2026-09-24 18:47:21 +0200

    feat(material): integrate slint material 3 component library

---
+++ build.rs
@@ -0,0 +1,14 @@
+use std::collections::HashMap;
+use std::env;
+use std::path::Path;
+
+fn main() {
+    let manifest_dir = env::var_os("CARGO_MANIFEST_DIR").unwrap();
+    let material_path = Path::new(&manifest_dir).join("material/material.slint");
+
+    let config = slint_build::CompilerConfiguration::new().with_library_paths(
+        HashMap::from([("material".to_string(), material_path)]),
+    );
+
+    slint_build::compile_with_config("ui/app.slint", config).unwrap();
+}
